870df5d87f05964a7e2ad9cdce393dce3e5d55c3
Your Name <you@example.com>
2020-03-28 14:55:25 +0100

    tempsave

--- HelperFunctions.js
+++ HelperFunctions.js
@@ -22,26 +22,27 @@ function fillHtmlCube(cube)
 //TODO in a loop with different field vars
 function fillHtmlSquare(square, placement, color)
 {
-
-//  for (var i = 0; i < 4; i++)
+  row = [ 0b0, 0b0, 0b0, 0b0 ];
+  for (var i = 0; i < 4; i++)
     for (var j = 0; j < 5; j++)
     {                 // aa 0 1
-      var rowTop = placement + "0" + j;
-      var rowLeft = placement + (4-j) + "0";
-      var rowBottom = placement + "4" + (4-j);
-      var rowRight = placement + j + "4";
+      row[0] = placement + "0" + (4-j);     //top
+      row[1] = placement + j + "0";   //left
+      row[2] = placement + "4" + j;       //bottom
+      row[3] = placement + (4-j) + "4";
       
-      if (!getBit(square[0], j))
-        document.getElementById(rowTop).style.backgroundColor = color;
       
-      if (!getBit(square[1], j))
-        document.getElementById(rowLeft).style.backgroundColor = color;
       
-      if (!getBit(square[2], j))
-        document.getElementById(rowBottom).style.backgroundColor = color;
+      console.log(placement + "0" + (4-j));     //top
+      console.log(placement + j + "0");   //left
+      console.log(placement + "4" + j);       //bottom
+      console.log(placement + (4-j) + "4");
       
-      if (!getBit(square[3], j))
-        document.getElementById(rowRight).style.backgroundColor = color;
+      if (!getBit(square[i], j))
+      {
+        console.log(row[i]);
+        document.getElementById(row[i]).style.backgroundColor = color;
+      }
     }
     
     printBitSquare(square); 
