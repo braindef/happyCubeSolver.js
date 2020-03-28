eb7bc2814ff75eae468e25d74abf023c2fbf871d
Your Name <you@example.com>
2020-03-28 14:44:57 +0100

    tempsave

--- Main.js
+++ Main.js
@@ -4,7 +4,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
   fillHtmlCube(blue);
 //  solveCube(blue);
-  console.log(dec2bin(flipSquare(blue[2])));
+  console.log(getBitRow(0b11010));
+
   for (var i = 0; i < 5; i++)
    console.log("----->"+getBit(0b11101, i));
    
